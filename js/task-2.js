04da865c69b7f1b005704eef2ea43ee1894d13c7
SuDenDimon <94171603+SuDenDimon@users.noreply.github.com>
2024-01-18 18:54:43 +0200

    task-2-edit
    поправки

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,8 @@
 function formatMessage(message, maxLength) {
     if (message.length <= maxLength){
         return (message);
-    } else {
-        return (`${message}...`);
+    } else if (message.length > maxLength){
+        return (`${message.slice(0, maxLength)}...`);
     }
 }
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
